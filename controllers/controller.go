429946f4d266d39a2fc6ea74d32c2a67c463c3d4
esmirs23 <esmira.abdullaeva@gmail.com>
2022-11-21 11:16:38 +0200

    500 error added

--- controllers/controller.go
+++ controllers/controller.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/esmira23/go-postgresql-docker/config"
 	"github.com/esmira23/go-postgresql-docker/csvparser"
 	"github.com/esmira23/go-postgresql-docker/models"
@@ -13,9 +15,11 @@ import (
 // @ID post
 // @Produce json
 // @Success 200 {object} models.ErrorMsg
+// @Failure 500 {object} models.ErrorMsg
 // @Router /post_data [post]
 func PostData(c *gin.Context) {
 	config.ConnectDB()
+	defer config.DB.Close()
 	tx := config.DB.MustBegin()
 
 	csvdata := csvparser.CSVparser()
@@ -24,9 +28,16 @@ func PostData(c *gin.Context) {
 		tx.MustExec("INSERT INTO example VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21) ON CONFLICT (transaction_id) DO NOTHING", csvparser.ToInt(csvdata[i][0]), csvparser.ToInt(csvdata[i][1]), csvparser.ToInt(csvdata[i][2]), csvparser.ToInt(csvdata[i][3]), csvparser.ToFloat(csvdata[i][4]), csvparser.ToFloat(csvdata[i][5]), csvparser.ToFloat(csvdata[i][6]), csvparser.ToFloat(csvdata[i][7]), csvparser.ToFloat(csvdata[i][8]), csvdata[i][9], csvdata[i][10], csvdata[i][11], csvdata[i][12], csvdata[i][13], csvparser.ToInt(csvdata[i][14]), csvdata[i][15], csvparser.ToInt(csvdata[i][16]), csvdata[i][17], csvparser.ToInt(csvdata[i][18]), csvdata[i][19], csvdata[i][20])
 	}
 
-	tx.Commit()
+	err := tx.Commit()
 
-	c.JSON(200, gin.H{"message": "data uploaded"})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+	} else {
+		c.JSON(http.StatusOK, models.ErrorMsg{
+			Message: "data uploaded",
+		})
+	}
+
 }
 
 // @Summary Get All
@@ -36,18 +47,24 @@ func PostData(c *gin.Context) {
 // @Produce json
 // @Success 200 {object} models.Data
 // @Failure 404 {object} models.ErrorMsg
+// @Failure 500 {object} map[string][]string
 // @Router /all [get]
 func GetAll(c *gin.Context) {
 	config.ConnectDB()
+	defer config.DB.Close()
 
 	items := []models.Data{}
 
 	err := config.DB.Select(&items, "SELECT * FROM example")
 
-	if len(items) == 0 || err != nil {
-		c.JSON(404, gin.H{"message": "data not found"})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+	} else if len(items) == 0 {
+		c.JSON(http.StatusNotFound, models.ErrorMsg{
+			Message: "data not found",
+		})
 	} else {
-		c.JSON(200, items)
+		c.JSON(http.StatusOK, items)
 	}
 }
 
@@ -59,18 +76,25 @@ func GetAll(c *gin.Context) {
 // @Param id path int true "Transaction ID"
 // @Success 200 {object} models.Data
 // @Failure 404 {object} models.ErrorMsg
+// @Failure 500 {object} map[string][]string
 // @Router /transaction/{id} [get]
 func GetByTransactionId(c *gin.Context) {
 	config.ConnectDB()
+	defer config.DB.Close()
 
 	items := []models.Data{}
 	param := c.Param("id")
+
 	err := config.DB.Select(&items, "SELECT * FROM example WHERE transaction_id = $1", param)
 
-	if len(items) == 0 || err != nil {
-		c.JSON(404, gin.H{"message": "data not found"})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+	} else if len(items) == 0 {
+		c.JSON(http.StatusNotFound, models.ErrorMsg{
+			Message: "data not found",
+		})
 	} else {
-		c.JSON(200, items)
+		c.JSON(http.StatusOK, items)
 	}
 }
 
@@ -82,9 +106,11 @@ func GetByTransactionId(c *gin.Context) {
 // @Param id query string true "Terminal ID. Pattern: id_1,id_2,..,id_n"
 // @Success 200 {object} models.Data
 // @Failure 404 {object} models.ErrorMsg
+// @Failure 500 {object} map[string][]string
 // @Router /terminal [get]
 func GetByTerminalId(c *gin.Context) {
 	config.ConnectDB()
+	defer config.DB.Close()
 
 	items := []models.Data{}
 	param := c.Query("id")
@@ -92,10 +118,14 @@ func GetByTerminalId(c *gin.Context) {
 	selectquery := "SELECT * FROM example WHERE terminal_id IN (" + param + ")"
 	err := config.DB.Select(&items, selectquery)
 
-	if len(items) == 0 || err != nil {
-		c.JSON(404, gin.H{"message": "data not found"})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+	} else if len(items) == 0 {
+		c.JSON(http.StatusNotFound, models.ErrorMsg{
+			Message: "data not found",
+		})
 	} else {
-		c.JSON(200, items)
+		c.JSON(http.StatusOK, items)
 	}
 }
 
@@ -107,19 +137,26 @@ func GetByTerminalId(c *gin.Context) {
 // @Param status path string true "Status (accepted/declined)"
 // @Success 200 {object} models.Data
 // @Failure 404 {object} models.ErrorMsg
+// @Failure 500 {object} map[string][]string
 // @Router /status/{status} [get]
 func GetByStatus(c *gin.Context) {
 	config.ConnectDB()
+	defer config.DB.Close()
 
 	items := []models.Data{}
 	param := c.Param("status")
 
 	err := config.DB.Select(&items, "SELECT * FROM example WHERE status = $1", param)
+	defer config.DB.Close()
 
-	if len(items) == 0 || err != nil {
-		c.JSON(404, gin.H{"message": "data not found"})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+	} else if len(items) == 0 {
+		c.JSON(http.StatusNotFound, models.ErrorMsg{
+			Message: "data not found",
+		})
 	} else {
-		c.JSON(200, items)
+		c.JSON(http.StatusOK, items)
 	}
 }
 
@@ -131,19 +168,26 @@ func GetByStatus(c *gin.Context) {
 // @Param type path string true "Payment Type (card/cash)"
 // @Success 200 {object} models.Data
 // @Failure 404 {object} models.ErrorMsg
+// @Failure 500 {object} map[string][]string
 // @Router /payment_type/{type} [get]
 func GetByPaymentType(c *gin.Context) {
 	config.ConnectDB()
+	defer config.DB.Close()
 
 	items := []models.Data{}
 	param := c.Param("type")
 
 	err := config.DB.Select(&items, "SELECT * FROM example WHERE payment_type = $1", param)
+	defer config.DB.Close()
 
-	if len(items) == 0 || err != nil {
-		c.JSON(404, gin.H{"message": "data not found"})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+	} else if len(items) == 0 {
+		c.JSON(http.StatusNotFound, models.ErrorMsg{
+			Message: "data not found",
+		})
 	} else {
-		c.JSON(200, items)
+		c.JSON(http.StatusOK, items)
 	}
 }
 
@@ -156,9 +200,11 @@ func GetByPaymentType(c *gin.Context) {
 // @Param to query string true "Date to YYYY-MM-DD"
 // @Success 200 {object} models.Data
 // @Failure 404 {object} models.ErrorMsg
+// @Failure 500 {object} map[string][]string
 // @Router /date [get]
 func GetByDatePost(c *gin.Context) {
 	config.ConnectDB()
+	defer config.DB.Close()
 
 	items := []models.Data{}
 	from := c.Query("from")
@@ -166,10 +212,14 @@ func GetByDatePost(c *gin.Context) {
 
 	err := config.DB.Select(&items, "SELECT * FROM example WHERE date_post::text BETWEEN $1 AND $2", from, to)
 
-	if len(items) == 0 || err != nil {
-		c.JSON(404, gin.H{"message": "data not found"})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+	} else if len(items) == 0 {
+		c.JSON(http.StatusNotFound, models.ErrorMsg{
+			Message: "data not found",
+		})
 	} else {
-		c.JSON(200, items)
+		c.JSON(http.StatusOK, items)
 	}
 }
 
@@ -181,18 +231,25 @@ func GetByDatePost(c *gin.Context) {
 // @Param narrative path string true "Partially specified payment narrative"
 // @Success 200 {object} models.Data
 // @Failure 404 {object} models.ErrorMsg
+// @Failure 500 {object} map[string][]string
 // @Router /payment_narrative/{narrative} [get]
 func GetByPaymentNarrative(c *gin.Context) {
 	config.ConnectDB()
+	defer config.DB.Close()
 
 	items := []models.Data{}
 	param := c.Param("narrative")
 
 	err := config.DB.Select(&items, "SELECT * FROM example WHERE payment_narrative ILIKE $1", "%"+param+"%")
+	defer config.DB.Close()
 
-	if len(items) == 0 || err != nil {
-		c.JSON(404, gin.H{"message": "data not found"})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+	} else if len(items) == 0 {
+		c.JSON(http.StatusNotFound, models.ErrorMsg{
+			Message: "data not found",
+		})
 	} else {
-		c.JSON(200, items)
+		c.JSON(http.StatusOK, items)
 	}
 }
